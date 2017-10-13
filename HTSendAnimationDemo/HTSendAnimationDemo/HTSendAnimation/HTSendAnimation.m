4e60116aaf85b155bcc77ca69cf588640c4e3762
chenghong <chenghong@chenghongtekimac-mini.local>
2017-10-13 16:33:31 +0800

    HT

--- HTSendAnimationDemo/HTSendAnimationDemo/HTSendAnimation/HTSendAnimation.m
+++ HTSendAnimationDemo/HTSendAnimationDemo/HTSendAnimation/HTSendAnimation.m
@@ -11,10 +11,10 @@
 
 @interface HTSendAnimation ()
 
-@property (nonatomic, strong) UIImageView *imageView;
-@property (nonatomic, strong) UIImageView *circle1;
-@property (nonatomic, strong) UIImageView *circle2;
-@property (nonatomic, strong) UIImageView *circle3;
+@property (nonatomic, strong) UIImageView *imageView;  // 主体图片
+@property (nonatomic, strong) UIImageView *circle1;  // 圆点1
+@property (nonatomic, strong) UIImageView *circle2;  // 圆点2
+@property (nonatomic, strong) UIImageView *circle3;  // 圆点3
 
 @end
 
@@ -22,16 +22,19 @@
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
+        // 初始化视图
         self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 0.5 * SCREEN_WIDTH, 0.4 * SCREEN_WIDTH)];
         [self.imageView setCenter:CGPointMake(SCREEN_WIDTH/2, SCREEN_HEIGHT - self.imageView.frame.size.height)];
         self.imageView.image = [UIImage imageNamed:@"envelope"];
         self.imageView.userInteractionEnabled = YES;
         [self addSubview:self.imageView];
         
+        // 设置向上轻扫手势触发事件
         UISwipeGestureRecognizer *swipeGes = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(didSwipeGes)];
         swipeGes.direction = UISwipeGestureRecognizerDirectionUp;
         [self.imageView addGestureRecognizer:swipeGes];
         
+        // 点击移除发送视图
         UITapGestureRecognizer *tapGes = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(removeSelf)];
         [self addGestureRecognizer:tapGes];
         
@@ -54,11 +57,14 @@
     return self;
 }
 
+// 向上轻扫事件
 - (void)didSwipeGes {
+    // 移除圆点
     [self.circle1 removeFromSuperview];
     [self.circle2 removeFromSuperview];
     [self.circle3 removeFromSuperview];
     
+    // 旋转并缩小，直至消失
     [UIView animateWithDuration:1.5 animations:^{
         self.imageView.frame = CGRectMake(SCREEN_WIDTH/2, 0, 0, 0);
         self.imageView.transform = CGAffineTransformMakeRotation(179);
@@ -68,6 +74,7 @@
     }];
 }
 
+// 移除发送视图
 - (void)removeSelf {
     [self removeFromSuperview];
 }
